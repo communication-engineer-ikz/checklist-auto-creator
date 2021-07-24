430748cff9ad1fab91210ef89d110219a5421a37
ikz <75650546+communication-engineer-ikz@users.noreply.github.com>
2021-07-24 21:52:12 +0900

    交互の背景色を設定する処理を追加

--- checklist-auto-creator.js
+++ checklist-auto-creator.js
@@ -20,4 +20,17 @@ function checklistAutoCreator() {
         sheet.deleteColumns(lastColumn + 1, maxColumn - lastColumn - 1); //チェックボックスを追加する列の確保
 
     }
+
+/* 参考
+ https://qiita.com/yamaotoko4177/items/4474217c18cc864bcc62
+*/
+    const targetRange = sheet.getRange(1, 1, lastRow, lastRow + 1);
+
+    console.log(targetRange.getBandings());
+
+    if (targetRange.getBandings()[0] != null) {
+        console.log("交互の背景色は適用できません");
+    } else {
+        targetRange.applyRowBanding(SpreadsheetApp.BandingTheme.GREEN);
+    }
 }
