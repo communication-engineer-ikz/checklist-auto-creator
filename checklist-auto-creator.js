96c937b24c5d40594bfe0182df77da81d63e82f0
ikz <75650546+communication-engineer-ikz@users.noreply.github.com>
2021-10-01 18:03:28 +0900

    やりたいことをコメント

--- checklist-auto-creator.js
+++ checklist-auto-creator.js
@@ -13,6 +13,12 @@ function checklistAutoCreator() {
     const lastColumn = sheet.getLastColumn();
     const maxColumn = sheet.getMaxColumns();
 
+    //CSV ファイルの値をGSS へコピー
+        //まだ取り込んでいないCSV ファイルを見つける
+        //CSV ファイル取り込み
+        //GSS へ転記
+
+    //余分なセルの削除
     if (maxRow - lastRow > 0) {
         sheet.deleteRows(lastRow + 1, maxRow - lastRow);
     }
@@ -21,6 +27,7 @@ function checklistAutoCreator() {
         sheet.deleteColumns(lastColumn + 1, maxColumn - lastColumn - 1); //チェックボックスを追加する列の確保
     }
 
+    //交互の背景色の適用
     /* 参考
         https://qiita.com/yamaotoko4177/items/4474217c18cc864bcc62
     */
@@ -32,6 +39,7 @@ function checklistAutoCreator() {
         targetRange.applyRowBanding(SpreadsheetApp.BandingTheme.GREEN);
     }
 
+    //一番右の列にチェックボックスを追加する
     /* 参考
         https://caymezon.com/gas-checkbox/#toc3
     */
